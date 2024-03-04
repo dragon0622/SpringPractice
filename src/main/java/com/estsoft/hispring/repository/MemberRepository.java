9f6cc503d71755723422f11f99da58020f2b88f5
dragon0622 <iamdragon.dev@gmail.com>
2024-03-04 11:06:36 +0900

    Feat : 매핑을 위한 인터페이스 파일 생성
    해당 인터페이스는 DB에서 데이터를 가져오는 퍼시스턴트 계층 역할을 함.

--- src/main/java/com/estsoft/hispring/repository/MemberRepository.java
+++ src/main/java/com/estsoft/hispring/repository/MemberRepository.java
@@ -1,10 +1,13 @@
 package com.estsoft.hispring.repository;
 
+import com.estsoft.hispring.controller.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //JPA
+//member 테이블에 접근해서 Member 클래스에 매핑하는 구현체
 
 @Repository
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
