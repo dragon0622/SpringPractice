27df111906d23b458fff783b81c8fb51cf251b9b
dragon0622 <iamdragon.dev@gmail.com>
2024-03-04 13:48:26 +0900

    Feat : Getter 및 Setter를 위한 DTO생성

---
+++ src/main/java/com/estsoft/hispring/controller/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.estsoft.hispring.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MemberDTO {
+    private long id;
+    private String name;
+}
