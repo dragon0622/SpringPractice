af471c13bb287735be6cc35880bc60f9bdb2d3b4
dragon0622 <iamdragon.dev@gmail.com>
2024-03-05 10:23:04 +0900

    Feat : DTO 수정

--- src/main/java/com/estsoft/hispring/controller/BookController.java
+++ src/main/java/com/estsoft/hispring/controller/BookController.java
@@ -1,14 +1,20 @@
 package com.estsoft.hispring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class BookController {
 
     @GetMapping("/books")
-    public String getbooks() {
+    public String getbooks(Model model) {
+        BookDTO bookDTO = new BookDTO("123","가나다","김정용");
+        model.addAttribute("bookList", List.of(bookDTO));
         return "bookManager";
     }
 
