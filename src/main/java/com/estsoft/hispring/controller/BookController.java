13843e1b8f0d4a6c21deb22c035ee09ca2ffcfa2
dragon0622 <iamdragon.dev@gmail.com>
2024-03-05 10:40:07 +0900

    Feat : 내부 객체로 변경
    객체를 생성해서 생성자에 넣었던 형태를 내부 객체로 보관

--- src/main/java/com/estsoft/hispring/controller/BookController.java
+++ src/main/java/com/estsoft/hispring/controller/BookController.java
@@ -1,20 +1,27 @@
 package com.estsoft.hispring.controller;
 
+import com.estsoft.hispring.repository.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.print.Book;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class BookController {
+    private final BookRepository bookRepository;
+    public BookController(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
 
     @GetMapping("/books")
     public String getbooks(Model model) {
-        BookDTO bookDTO = new BookDTO("123","가나다","김정용");
-        model.addAttribute("bookList", List.of(bookDTO));
+        List<BookDTO> bookList = bookRepository.getAllBooks();
+//        BookDTO bookDTO = new BookDTO("123","가나다","김정용");
+        model.addAttribute("bookList", List.of(bookList));
         return "bookManager";
     }
 
---
+++ src/main/java/com/estsoft/hispring/repository/BookRepository.java
@@ -0,0 +1,28 @@
+package com.estsoft.hispring.repository;
+
+import com.estsoft.hispring.controller.BookDTO;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class BookRepository {
+    private Map<String, BookDTO> bookDTOMap;
+
+    public BookRepository() {
+        bookDTOMap = new HashMap<>();
+        BookDTO bookDTO = new BookDTO("123", "가나다", "김정용");
+        bookDTOMap.put(bookDTO.getId(), bookDTO);
+    }
+
+    public List<BookDTO> getAllBooks() {
+        List<BookDTO> bookDTOList = new ArrayList<>();
+        for (Map.Entry<String, BookDTO> book : bookDTOMap.entrySet()) {
+            bookDTOList.add(book.getValue());
+        }
+        return bookDTOList;
+    }
+}
