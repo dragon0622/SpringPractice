2d1aa3c7581d9470dec67e1e4ebda434c4d6b467
dragon0622 <iamdragon.dev@gmail.com>
2024-03-05 10:05:14 +0900

    Feat : getter,setter,생정자, 필드 추가

--- src/main/java/com/estsoft/hispring/controller/BookDTO.java
+++ src/main/java/com/estsoft/hispring/controller/BookDTO.java
@@ -1,4 +1,16 @@
 package com.estsoft.hispring.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookDTO {
+    private String isbn;
+    private String name;
+    private String author;
 }
